aaba929d00f9044a100a52d2b2fa4273dbcb1823
Brad Camroux <bgcamroux@gmail.com>
2017-02-03 22:21:34 -0700

    Updates for readability & most recent work
    Took out some irrelevant material and code that didn't end up working. Put in a few extra comments for readability.

--- 02_FranckHertz_Method1.py
+++ 02_FranckHertz_Method1.py
@@ -6,21 +6,11 @@ Created on Thu Jan 26 18:00 2017
 @author: brad
 """
 
-import os
-
-if os.getlogin()=='brad':
-    os.chdir('/home/brad/Documents/UAlberta/W17/PHYS297/Franck-Hertz/Analysis/')
-elif os.getlogin()=='camroux':
-    os.chdir('/media/D43F-C3DF/PHYS297/Franck-Hertz/Analysis/')
-else:
-    os.chdir('/home/')
-
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd   ## PANDAS is glorious and makes handling data easier, much like in R!
 #from scipy import stats
 from scipy import signal as sig
-
 import BGCprocLib as bgc
 
 ## Set order for peak finding routine (Default should be 10)
@@ -63,7 +53,7 @@ vaic  = ['Va', 'Ic']
 ## Set up a new, empty dataframe indexed on temps and dirs. Unfortunately, for now, 
 ## we have to set this up manually
 idx = pd.MultiIndex.from_tuples([(152, 'Up'), (161, 'Up'), (172, 'Up'), (178, 'Up'), (180, 'Up'), (190, 'Up'), (152, 'Dn'), (161, 'Dn'), (172, 'Dn'), (178, 'Dn'), (180, 'Dn'), (190, 'Dn')], names=['Temps','Dirs'])
-smoothed = pd.DataFrame(columns=['Va','Ic'], index=idx)
+sm = pd.DataFrame(index=idx)
 for i in temps:
     print("\n==================================================")
     print("processing temperature: ",i)
@@ -73,21 +63,25 @@ for i in temps:
             print("     processing variable: ",v)
             print("        min ", v, np.min(AllData.loc[i].loc[d][v]))
             print("        max ", v, np.max(AllData.loc[i].loc[d][v]))
-            x = bgc.funcSmoothFilt(winsize, wintype, winstdv, AllData.loc[i].loc[d][v], rollbak)
-            smoothed.loc[i].loc[d][v] = x
-
+            sm.loc[i].loc[d][v] = bgc.funcSmoothFilt(winsize, wintype, winstdv, AllData.loc[i].loc[d][v], rollbak)
+            ## This ran without error, but resulted in an empty dataframe :(
+            ## I know there is a way to access locations using, say, sm.loc[(i,d),v], but I can't get that to work :(
+            
+## This is as far as I need to go for now.
+######################################################################################################
+##
 ## Find the local maxima in the smoothed data. We want to make sure that we find maxima
 ## so we set the order to 10. This makes sure that the value returned is the maximum
 ## out of 10 points on either side.
-peaksUp = sig.argrelmax(-yfiltUp, order=Ord)
-peaksDn = sig.argrelmax(-yfiltDn, order=Ord)
+#peaksUp = sig.argrelmax(-yfiltUp, order=Ord)
+#peaksDn = sig.argrelmax(-yfiltDn, order=Ord)
 
 ## Adjust the data as necessary
 ## Make sure to take a look at the accelerating potentials for each peak to make
 ## sure they make sense. If necessary, increase "Ord", and only as a last resort
 ## should we delete elements of the peaks vector(s).
-a1u = [0] ## vector of indices to be deleted from the peaksUp and peaksDn results
-peaksUp = np.delete(peaksUp, a1u)
+#a1u = [0] ## vector of indices to be deleted from the peaksUp and peaksDn results
+#peaksUp = np.delete(peaksUp, a1u)
 #a2u = [0,1,2,3,4,6,14]
 #a2d = [0]
 #peaksUp = np.delete(peaksUp, a2u)
@@ -105,30 +99,30 @@ peaksUp = np.delete(peaksUp, a1u)
 ##################################
 ### Let's see what these data look like!
 ### Waveform first
-plt.figure(figsize=(12,6))
-plt.clf()
-plt.subplot(1,2,1)
-plt.plot(xfiltUp, -yfiltUp, 'b^', markersize=2, visible=True)
-plt.plot(xfiltDn, -yfiltDn, 'gv', markersize=2, visible=True)
-plt.plot(xfiltUp[peaksUp], -yfiltUp[peaksUp], 'bd', ls='none')
-plt.plot(xfiltDn[peaksDn], -yfiltDn[peaksDn], 'gd', ls='none')
+#plt.figure(figsize=(12,6))
+#plt.clf()
+#plt.subplot(1,2,1)
+#plt.plot(xfiltUp, -yfiltUp, 'b^', markersize=2, visible=True)
+#plt.plot(xfiltDn, -yfiltDn, 'gv', markersize=2, visible=True)
+#plt.plot(xfiltUp[peaksUp], -yfiltUp[peaksUp], 'bd', ls='none')
+#plt.plot(xfiltDn[peaksDn], -yfiltDn[peaksDn], 'gd', ls='none')
 ## Give title to plot and axes, add a legend
-plt.xlabel(r'Accelerating Potential, $V_a$ ($V$)')
-plt.ylabel(r'Collector Current, $I_c$ ($\times 10^{-10}A$)')
-plt.title(r'Smoothed Franck-Hertz experiment results')
+#plt.xlabel(r'Accelerating Potential, $V_a$ ($V$)')
+#plt.ylabel(r'Collector Current, $I_c$ ($\times 10^{-10}A$)')
+#plt.title(r'Smoothed Franck-Hertz experiment results')
 #plt.legend(handles=[pt1, pt2, pt3, pt4], loc=2, numpoints=5)
 ## Save the figure to disk
 #plt.savefig(figName1)
 
 ### Then the peak potentials vs peak number
-plt.subplot(1,2,2)
-plt.plot(np.sort(xfiltUp[peaksUp]), 'b^')
-plt.plot(np.sort(xfiltDn[peaksDn]), 'gv')
+#plt.subplot(1,2,2)
+#plt.plot(np.sort(xfiltUp[peaksUp]), 'b^')
+#plt.plot(np.sort(xfiltDn[peaksDn]), 'gv')
 #plt.xlim(-0.5,12.5)
 #plt.ylim( 0.0,70.0)
-plt.xlabel(r'Peak Number')
-plt.ylabel(r'Accelerating Potential, $V_a$ ($V$)')
-plt.title(r'Accelerating potential as a function of peak number')
+#plt.xlabel(r'Peak Number')
+#plt.ylabel(r'Accelerating Potential, $V_a$ ($V$)')
+#plt.title(r'Accelerating potential as a function of peak number')
 ## Save the figure to disk
 #plt.savefig(figName2)
 
